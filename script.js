8627fce774f6ec1d67a1ef3038f908d1f290b35c
Miquel <abellagarciamiquel@gmail.com>
2022-12-12 16:43:54 +0100

    added listener to view more button

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
 const postsContainer = document.getElementById("posts-container");
 
-
 function getPosts() {
   fetch("http://localhost:3000/posts")
-  .then((response) => response.json())
+    .then((response) => response.json())
     .then((posts) => {
-        console.log(posts)
-        posts.forEach((post, index) => {
-            fetch(`http://localhost:3000/images/${index}`)
-            .then((response) => response.json())
-            .then((image) => {
-                
+      console.log(posts);
+      posts.forEach((post, index) => {
+        fetch(`http://localhost:3000/images/${index}`)
+          .then((response) => response.json())
+          .then((image) => {
             let newPost = document.createElement("div");
             newPost.classList.add("card");
             newPost.classList.add("m-4");
@@ -20,22 +18,18 @@ function getPosts() {
             <div class="card-body">
             <p class="card-text">${post.title}</p>
             </div>
-            <button type="button" userId='${post.userId}' postId='${post.id}' class="btn btn-primary view-more-btn" data-bs-toggle="modal" data-bs-target="#exampleModal">
+            <button type="button" onclick="getInfo(event)" userId='${post.userId}' postId='${post.id}' class="btn btn-primary view-more-btn" data-bs-toggle="modal" data-bs-target="#exampleModal">
             View More
             </button> 
             `;
             postsContainer.appendChild(newPost);
-        });
+          });
+      });
     });
-});
-  let viewMoreBtn = document.getElementsByClassName("view-more-btn");
-   for (let btn of viewMoreBtn){
-      btn.addEventListener('click', getInfo)
-   }
 }
 
 getPosts();
 
-function getInfo(){
-    console.log('click')
+function getInfo(event) {
+  console.log(event.target);
 }
