c1d457ccd753c972a3aaa21f208702aa8c7dd504
Berta Garcia <bertagnavarro93@gmail.com>
2022-12-12 16:36:25 +0100

    post btn added

--- script.js
+++ script.js
@@ -1,29 +1,41 @@
 const postsContainer = document.getElementById("posts-container");
 
+
 function getPosts() {
   fetch("http://localhost:3000/posts")
-    .then((response) => response.json())
+  .then((response) => response.json())
     .then((posts) => {
-
-      posts.forEach((post, index) => {
-        fetch(`http://localhost:3000/images/${index}`)
-          .then((response) => response.json())
-          .then((image) => {
-          
+        console.log(posts)
+        posts.forEach((post, index) => {
+            fetch(`http://localhost:3000/images/${index}`)
+            .then((response) => response.json())
+            .then((image) => {
+                
             let newPost = document.createElement("div");
             newPost.classList.add("card");
             newPost.classList.add("m-4");
             newPost.style.width = "18rem";
             newPost.innerHTML = `
             <img src="${image.download_url}" class="card-img-top" alt="imatge!">
-                            <div class="card-body">
-                            <p class="card-text">${post.title}</p>
-                            </div>
-                            `;
-                            postsContainer.appendChild(newPost);
-                        });
-      });
+            <div class="card-body">
+            <p class="card-text">${post.title}</p>
+            </div>
+            <button type="button" userId='${post.userId}' postId='${post.id}' class="btn btn-primary view-more-btn" data-bs-toggle="modal" data-bs-target="#exampleModal">
+            View More
+            </button> 
+            `;
+            postsContainer.appendChild(newPost);
+        });
     });
+});
+  let viewMoreBtn = document.getElementsByClassName("view-more-btn");
+   for (let btn of viewMoreBtn){
+      btn.addEventListener('click', getInfo)
+   }
 }
 
 getPosts();
+
+function getInfo(){
+    console.log('click')
+}
