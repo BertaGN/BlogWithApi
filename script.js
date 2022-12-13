1fb1fa2061b7ea596dd4ad8211ff462d43aa669a
Miquel <abellagarciamiquel@gmail.com>
2022-12-13 13:25:26 +0100

    delete modal implemented

--- script.js
+++ script.js
@@ -14,142 +14,127 @@ loadComments.addEventListener("click", toggleCommentsVisibility);
 saveUpdateModalBtn.addEventListener("click", updatePost);
 
 function getPosts() {
-    fetch("http://localhost:3000/posts")
-        .then((response) => response.json())
-        .then((posts) => {
-            posts.forEach((post) => {
-                fetch(`http://localhost:3000/images/${post.id}`)
-                    .then((response) => response.json())
-                    .then((image) => {
-                        let newPost = document.createElement("div");
-                        newPost.classList.add("card");
-                        newPost.classList.add("m-4");
-                        newPost.style.width = "18rem";
-                        newPost.id = post.id;
-                        newPost.innerHTML = `
+  fetch("http://localhost:3000/posts")
+    .then((response) => response.json())
+    .then((posts) => {
+      posts.forEach((post) => {
+        fetch(`http://localhost:3000/images/${post.id}`)
+          .then((response) => response.json())
+          .then((image) => {
+            let newPost = document.createElement("article");
+            newPost.classList.add("card");
+            newPost.classList.add("m-4");
+            newPost.style.width = "23rem";
+            newPost.id = post.id;
+            newPost.innerHTML = `
             <img src="${image.download_url}" class="card-img-top" alt="imatge! load="lazy"">
             <div class="card-body">
-            <p class="card-text" id="main-post-title-${post.id}">${post.title}</p>
+              <p class="card-text" id="main-post-title-${post.id}">${post.title}</p>
             </div>
             <div class="btn-group" role="group">
-            <button type="button" onclick="getInfo(event)" userId='${post.userId}' postId='${post.id}' class="btn btn-outline-primary view-more-btn" data-bs-toggle="modal" data-bs-target="#exampleModal">
-            View More
-            </button> 
-            <button type="button" postId='${post.id}' onclick="deletePost(event)" class="btn btn-outline-danger">Delete</button>
-            <button type="button" onclick="getUpdateModalInfo(event)" postId='${post.id}' class="btn btn-outline-warning view-more-btn" data-bs-toggle="modal" data-bs-target="#updateModal"> 
-            Update
-            </button> 
+              <button type="button" onclick="getInfo(event)" userId='${post.userId}' postId='${post.id}' class="btn btn-outline-info view-more-btn" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                <img src="https://www.shareicon.net/data/2015/09/20/104311_add_512x512.png" style="height:25px; pointer-events:none"/>
+              </button> 
+              <button type="button" onclick="getUpdateModalInfo(event)" postId='${post.id}' class="btn btn-outline-warning view-more-btn" data-bs-toggle="modal" data-bs-target="#updateModal"> 
+                <img src="https://icons.veryicon.com/png/o/miscellaneous/linear-small-icon/edit-246.png" style="height:25px; pointer-events:none"/>
+              </button> 
+              <button type="button" postId='${post.id}' onclick="deletePost(event)" class="btn btn-outline-danger" data-bs-toggle="modal" data-bs-target="#deleted-successfully">
+                <img src="https://cdn-icons-png.flaticon.com/512/1214/1214428.png" style="height:25px; pointer-events:none"/>
+              </button>
             </div>
-            
             `;
-                        postsContainer.appendChild(newPost);
-                    });
-            });
-        });
+            postsContainer.appendChild(newPost);
+          });
+      });
+    });
 }
 
 getPosts();
 
 function getInfo(event) {
-    let userId = event.target.getAttribute("userId");
-    let postId = event.target.getAttribute("postId");
+  let userId = event.target.getAttribute("userId");
+  let postId = event.target.getAttribute("postId");
 
-    fetch(`http://localhost:3000/posts/${postId}`)
-        .then((response) => response.json())
-        .then((data) => {
-            postModalTitle.innerText = data.title;
-            postModalBody.innerText = data.body;
-        });
-    fetch(`http://localhost:3000/users/${userId}`)
-        .then((response) => response.json())
-        .then((data) => {
-            postModalUserName.innerText = data.username + " - " + data.name;
-            postModalUserEmail.innerText = data.email;
-        });
-    fetch(`http://localhost:3000/posts/${postId}/comments`)
-        .then((response) => response.json())
-        .then((data) => {
-            data.map(function (comment) {
-                postModalComments.innerHTML += ` <p style="font-weight:bold;">${comment.name}</p>
+  fetch(`http://localhost:3000/posts/${postId}`)
+    .then((response) => response.json())
+    .then((data) => {
+      postModalTitle.innerText = data.title;
+      postModalBody.innerText = data.body;
+    });
+  fetch(`http://localhost:3000/users/${userId}`)
+    .then((response) => response.json())
+    .then((data) => {
+      postModalUserName.innerText = data.username + " - " + data.name;
+      postModalUserEmail.innerText = data.email;
+    });
+  fetch(`http://localhost:3000/posts/${postId}/comments`)
+    .then((response) => response.json())
+    .then((data) => {
+      data.map(function (comment) {
+        postModalComments.innerHTML += ` <p style="font-weight:bold;">${comment.name}</p>
             <p>${comment.body}</p>
             <p>${comment.email}</p>
             <hr style="border: 1px solid black;">
             `;
-            });
-        });
-}
-
-function toggleCommentsVisibility(e) {
-    postModalComments.classList.toggle("d-none");
-    if (postModalComments.classList.contains("d-none")) {
-        loadComments.innerText = "Load Comments";
-    } else {
-        loadComments.innerText = "Hide Comments";
-    }
-}
-
-function deletePost(e) {
-    let postId = parseInt(e.target.getAttribute("postId"));
-
-    fetch(`http://localhost:3000/posts/${postId}`, {
-        method: "DELETE",
-    }).then(() => document.getElementById(postId).remove());
-
-    fetch(`http://localhost:3000/images/${postId}`, {
-        method: "DELETE",
+      });
     });
 }
 
+function toggleCommentsVisibility(e) {
+  postModalComments.classList.toggle("d-none");
+  if (postModalComments.classList.contains("d-none")) {
+    loadComments.innerText = "Load Comments";
+  } else {
+    loadComments.innerText = "Hide Comments";
+  }
+}
 
+function deletePost(e) {
+  let postId = parseInt(e.target.getAttribute("postId"));
 
-//   fetch(`http://localhost:3000/posts/${postId}`, {
-//   method: 'PUT',
-//   body: JSON.stringify({
-//     id: postId,
-//     title: 'foo',
-//     body: 'bar',
-//     userId: 1,
-//   }),
-//   headers: {
-//     'Content-type': 'application/json; charset=UTF-8',
-//   },
-// })
-//   .then((response) => response.json())
-//   .then((json) => console.log(json));
+  fetch(`http://localhost:3000/posts/${postId}`, {
+    method: "DELETE",
+  }).then((response) => {
+    if (response.status !== 200) {
+      return;
+    } else {
+      document.getElementById(postId).remove();
+    }
+  });
 
-//   console.log(postId);
-// }
+  fetch(`http://localhost:3000/images/${postId}`, {
+    method: "DELETE",
+  });
+}
 
 function getUpdateModalInfo(event) {
-    const postId = event.target.getAttribute("postId");
-    saveUpdateModalBtn.setAttribute("postId", postId);
-    fetch(`http://localhost:3000/posts/${postId}`)
-        .then((response) => response.json())
-        .then((data) => {
-            updateModalTitle.value = data.title;
-            updateModalText.value = data.body;
-            userId = data.userId
-        });
+  const postId = event.target.getAttribute("postId");
+  saveUpdateModalBtn.setAttribute("postId", postId);
+  fetch(`http://localhost:3000/posts/${postId}`)
+    .then((response) => response.json())
+    .then((data) => {
+      updateModalTitle.value = data.title;
+      updateModalText.value = data.body;
+      userId = data.userId;
+    });
 }
 
 function updatePost(event) {
-    const postId = event.target.getAttribute("postId");
-    let mainPostTitle = document.getElementById(`main-post-title-${postId}`)
-    console.log(userId)
-    fetch(`http://localhost:3000/posts/${postId}`, {
-        method: 'PUT',
-        body: JSON.stringify({
-            id: postId,
-            title: updateModalTitle.value,
-            body: updateModalText.value,
-            userId: userId,
-        }),
-        headers: {
-            'Content-type': 'application/json; charset=UTF-8',
-        },
-    })
-        .then((response) => response.json())
-        .then((json) => mainPostTitle.innerText = json.title);
-
-
+  const postId = event.target.getAttribute("postId");
+  let mainPostTitle = document.getElementById(`main-post-title-${postId}`);
+  console.log(userId);
+  fetch(`http://localhost:3000/posts/${postId}`, {
+    method: "PUT",
+    body: JSON.stringify({
+      id: postId,
+      title: updateModalTitle.value,
+      body: updateModalText.value,
+      userId: userId,
+    }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  })
+    .then((response) => response.json())
+    .then((json) => (mainPostTitle.innerText = json.title));
 }
